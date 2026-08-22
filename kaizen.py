4c68d97db68c134d30106d2c43ae8da400b59eaa
shivraj <shivrajgulbal@gmail.com>
2026-08-22 20:15:11 +0530

    Working agent loop with 2nd new  tool calling

--- kaizen.py
+++ kaizen.py
@@ -1,4 +1,5 @@
 import requests
+import os
 
 payload = {"convo":[],"tools":[{"type": "function",
                                     "function": {
@@ -12,7 +13,20 @@ payload = {"convo":[],"tools":[{"type": "function",
                                         "required": ["path"]
                                         }
                                     }
-                }]}
+                },
+                {"type":"function",
+                    "function":{
+                        "name": "list_file",
+                        "description": "lists the files in a directory given its path",
+                        "parameters": {
+                            "type": "object",
+                            "properties": {
+                            "path": {"type": "string", "description": "path to the directory"}
+                            },
+                        "required": ["path"]
+                        }
+                    }}
+                ]}
 
 def qwen(messages: dict):
     
@@ -21,19 +35,26 @@ def qwen(messages: dict):
                                    "messages": messages["convo"],"tools":messages["tools"],"stream":False})
 
     reply = response.json()
+    
     if "tool_calls" in reply["message"]:
         tool = reply["message"]["tool_calls"]
         argu = tool[0]["function"]["arguments"]
         name = tool[0]["function"]["name"]
         if name == "read_file":
-            print(f"[DEBUG] Model wants to read: {argu['path']!r}")
+            
             respon = read_file(argu["path"])
-            print(f"[DEBUG] read_file returned: {respon!r}")
+            
             payload["convo"].append({"role":"tool","content": respon})
             tool_response = qwen(payload)
             return tool_response
+        elif name == "list_file":
             
+            respon = list_file(argu["path"])
 
+            payload["convo"].append({"role":"tool","content": respon})
+            tool_response = qwen(payload)
+            return tool_response
+             
     return reply["message"]["content"]
 
 def read_file(path):
@@ -42,6 +63,19 @@ def read_file(path):
             return f.read()
     except FileNotFoundError:
         return f"File not found at{path}"
+
+def list_file(path):
+    try:
+        files = os.listdir(path)
+        return ",".join(files)
+    except FileNotFoundError:
+        return f"{path}, This path does not exist"
+    except NotADirectoryError:
+        return f"{path}, is not a directory"
+    except PermissionError:
+        return f"Permision Denied to access {path}"
+    except OSError as e:
+        return f"OS Error: {e}"
 
 def main():
     while True:
